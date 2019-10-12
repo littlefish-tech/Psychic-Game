ab9e68b7b9a23920b818eb2b495a8fafab0a01d3
Yimeng <yimeng.yu@gmail.com>
2019-10-12 16:39:39 -0700

    update README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Psychic-Game
+### I will think about functions the JS will execute 
+- I want there are 4 variables showing on the screen: 
+    1. Wins
+    2. Losses
+    3. Guesses Left
+    4. Your Guesses so far
+
+- Create a list for computer Choice(the list include 26 letters)
+- Make sure that even the user press a upper case letter, it will convert to a lower case letter
+- When user guess same as computer guess, I win
+- When user guess not same as computer guess, then the user will loss a chance of guess(the user total have 12 times for guessing)
+- When user lose all 12 times of guessing, the user loss
+
+
+###Technology I use:
+- HTML
+- Javascript
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,77 @@
+<script>
 // list of letters computer think
-var computerChoice = ["n", "p","r","l", "s"]
-// Create variables that hold references to the places in the HTML where we want to display things.
+var computerChoice = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-console.log(wins);
+
+// creat variables 
 var wins = 0;
 var losses = 0;
+var guessesLeft = 12;
+var yourGuessesSoFar = [];
 
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
+// create the variables for the ID link to the HTML
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var yourGuessesSoFarText = document.getElementById("yourGuessesSoFar-text");
+
+function updateDom() {
+winsText.textContent = wins;
+lossesText.textContent = losses;
+guessesLeftText.textContent = guessesLeft;
+yourGuessesSoFarText.textContent = yourGuessesSoFar;
+}
+
+updateDom()
+// wins++;
+// updateDom()
+
+
+
+
+
 // This function is run whenever the user presses a key
 document.onkeyup = function(event) {
 
-// Determines which ey was pressed.
-var userGuess = event.key;
+// Determines which key was pressed.
+var userGuess = event.key.toLowerCase();
+console.log(userGuess + "userguess")
+
 //Randonly chooses a choice from the optio array. This is the Computer's Guess.
-var computerGuess = computerChoice[Math.floor(Math.random()*computerChoice.length];
+var computerGuess = computerChoice[Math.floor(Math.random()*computerChoice.length)];
+console.log(computerGuess)
 // user guess letter vs company guess letter
-if ((userGuess ==="n") && (computerGuess === "n")) {
-    wins++;
+
+if (guessesLeft > 0) {
+if (computerChoice.indexOf(userGuess) === -1)
+alert("You must type a letter")
+if (userGuess === computerGuess) {
+wins++;
+yourGuessesSoFar.push(userGuess)
+updateDom()
+}else {
+guessesLeft--;
+yourGuessesSoFar.push(userGuess)
+updateDom()
 }
-if ((userGuess ==="r") && (computerGuess === "r")) {
-    wins++;
+}else {
+losses++;
+guessesLeft = 3;
+updateDom()
 }
 
-if ((userGuess ==="p") && (computerGuess === "p")) {
-    wins++;
-}
-if ((userGuess ==="l") && (computerGuess === "l")) {
-    wins++;
-}
-if ((userGuess ==="s") && (computerGuess === "s")) {
-    wins++;
-} else {
-    losses++;
-}
 //If I win, I will not loss attempts
 // I guessed the letter not in the list, I loss
 //If I loss, I will lose attempts
 
- // Hide the directions
- directionsText.textContent = "";
+// Hide the directions
+//  directionsText.textContent = "";
 
- // Display the user and computer guesses, and wins/losses/ties.
- userChoiceText.textContent = "You chose: " + userGuess;
- computerChoiceText.textContent = "The computer chose: " + computerGuess;
- winsText.textContent = "wins: " + wins;
- lossesText.textContent = "losses: " + losses;
+// Display the user and computer guesses, and wins/losses/ties.
+//  userChoiceText.textContent = "You chose: " + userGuess;
+//  computerChoiceText.textContent = "The computer chose: " + computerGuess;
+//  winsText.textContent = "wins: " + wins;
+//  lossesText.textContent = "losses: " + losses;
 }
+
+</script>
